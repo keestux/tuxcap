101db50deb26a0453f5b10180b1a84d60d1d4156
wpvanpaassen <wpvanpaassen>
2007-11-28 21:57:20 +0000

    adding python sources

---
+++ tuxcap-build/python-bindings/Template/game.py
@@ -0,0 +1,30 @@
+# Template pycap game v1.0
+# Written by Farbs
+
+appIni = {	"mCompanyName"		: "CompanyNameGoesHere",
+		"mFullCompanyName"	: "CompanyNameGoesHere",
+		"mProdName"		: "Pycap Template Game",
+		"mProductVersion"	: "1.0",
+		"mTitle"			: "Pycap Template Game v1.0",
+		"mRegKey"			: "PycapTest",
+		"mWidth"			: 800,
+		"mHeight"			: 600,
+		"mAutoEnable3D"	: 1,
+		"mVSyncUpdates"	: 1 }
+
+import Pycap as PC
+PCR = None
+
+def loadBase():
+	import PycapRes
+	global PCR
+	PCR = PycapRes
+
+def init():
+	pass
+
+def update( delta ):
+	PC.markDirty()
+
+def draw():
+	pass
