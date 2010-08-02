6fbd1505d9dd75f9c0bd64a2e9c4f58a4c736eb1
Kees Bakker <keesb@users.sourceforge.net>
2010-08-02 22:47:51 +0200

    Catch exceptions in python demo main()

--- src/pythondemo1/main.cpp
+++ src/pythondemo1/main.cpp
@@ -1,14 +1,23 @@
+#include <cstdio>
 #include "PycapApp.h"
 
 using namespace Sexy;
 
-int main(int argc, char** argv) {
-  PycapApp app;
+int main(int argc, char** argv)
+{
+    int exit_code = 0;
+    try {
+        PycapApp app;
 
-  SetAppResourceFolder("../images");
+        app.Init(argc, argv, false);
 
-  app.Init(argc, argv, false);
-  app.Start();
-  app.Shutdown();
-  return 0;
+        app.Start();
+        app.Shutdown();
+    } catch (Exception * e) {
+        fprintf(stderr, "%s\n", e->getMessage().c_str());
+    } catch (...) {
+        fprintf(stderr, "Oops. Unknown exception occured\n");
+        exit_code = 1;
+    }
+    return exit_code;
 }
