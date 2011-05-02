193df852eec6826a869b2adeeef27ee9018c5f7a
wpvanpaassen <wp.van.paassen@gmail.com>
2011-05-02 21:01:11 +0200

    Now parsing commandline in all demo's

--- src/physicsdemo6/main.cpp
+++ src/physicsdemo6/main.cpp
@@ -1,20 +1,39 @@
 #include <cstdio>
+#include <exception>
+#include <CommandLine.h>
 #include "GameApp.h"
 
 using namespace Sexy;
+using namespace std;
 
 int main(int argc, char** argv)
 {
     int exit_code = 0;
+
+    // Do the initial command line parsing.
+    if (!CmdLine::ParseCommandLine(argc, argv)) {
+        return exit_code;
+    }
+
     try {
         GameApp app;
 
+        if (app.ParseCommandLine(argc, argv) != 0) {
+            return exit_code;
+        }
+
         app.Init();
+
         app.Start();
         app.Shutdown();
-    } catch (Exception * e) {
+    }
+    catch (exception * e) {
+        fprintf(stderr, "%s\n", e->what());
+    }
+    catch (Exception * e) {
         fprintf(stderr, "%s\n", e->getMessage().c_str());
-    } catch (...) {
+    }
+    catch (...) {
         fprintf(stderr, "Oops. Unknown exception occured\n");
         exit_code = 1;
     }
--- src/pythondemo1/main.cpp
+++ src/pythondemo1/main.cpp
@@ -1,21 +1,39 @@
 #include <cstdio>
-#include "PycapApp.h"
+#include <exception>
+#include <CommandLine.h>
+#include "GameApp.h"
 
 using namespace Sexy;
+using namespace std;
 
 int main(int argc, char** argv)
 {
     int exit_code = 0;
-    try {
-        PycapApp app;
 
-        app.Init(argc, argv, false);
+    // Do the initial command line parsing.
+    if (!CmdLine::ParseCommandLine(argc, argv)) {
+        return exit_code;
+    }
+
+    try {
+        GameApp app;
+
+        if (app.ParseCommandLine(argc, argv) != 0) {
+            return exit_code;
+        }
+
+        app.Init();
 
         app.Start();
         app.Shutdown();
-    } catch (Exception * e) {
+    }
+    catch (exception * e) {
+        fprintf(stderr, "%s\n", e->what());
+    }
+    catch (Exception * e) {
         fprintf(stderr, "%s\n", e->getMessage().c_str());
-    } catch (...) {
+    }
+    catch (...) {
         fprintf(stderr, "Oops. Unknown exception occured\n");
         exit_code = 1;
     }
